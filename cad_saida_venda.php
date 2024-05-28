33b0e2b567d318f401bce4b2f890dc7461f77815
KovaFPS <kovafps1@gmail.com>
2024-05-28 10:09:42 -0300

    Discord, Att de Calculo e duplo registro

--- cad_saida_venda.php
+++ cad_saida_venda.php
@@ -5,36 +5,117 @@ include("conexao.php");
 
 $mensagem = ""; // Variável para armazenar mensagens de erro ou sucesso
 
-// Verificação de estoque antes do envio do formulário
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $produto_id = $_POST["produto_id"];
     $quantidade = $_POST["quantidade"];
+    $cliente_id = $_POST["cliente_id"];
+    $valor_unitario = $_POST["valor_unitario"];
 
-    // Lógica para verificar o estoque (substitua com sua implementação real)
-    $sql_estoque = "SELECT quantidade FROM produto WHERE id='$produto_id'";
+    // Verificação de estoque
+    $sql_estoque = "SELECT nome_produto, quantidade FROM produto WHERE id='$produto_id'";
     $result_estoque = mysqli_query($conexao, $sql_estoque);
     $produto = mysqli_fetch_assoc($result_estoque);
 
     if ($produto) {
         if ($produto["quantidade"] >= $quantidade) {
-            // Se o estoque for suficiente, continuar com o processamento do formulário
-            $cliente_id = $_POST["cliente_id"];
-            $valor_unitario = $_POST["valor_unitario"];
+            $nova_quantidade = $produto["quantidade"] - $quantidade;
+            $valor_total = $quantidade * $valor_unitario;
+            $usuario_id = obterIdUsuarioLogado();
+            $nome_produto = $produto['nome_produto'];
+
+            // Obter o nome do cliente
+            $sql_cliente = "SELECT nome FROM cliente WHERE id='$cliente_id'";
+            $result_cliente = mysqli_query($conexao, $sql_cliente);
+            $cliente = mysqli_fetch_assoc($result_cliente);
+            $nome_cliente = $cliente['nome'];
+
+            // Descrição da transação financeira
+            $descricao_financa = "Venda de $quantidade x $nome_produto para $nome_cliente";
 
             // Atualizar o estoque
-            $nova_quantidade = $produto["quantidade"] - $quantidade;
             $sql_update_estoque = "UPDATE produto SET quantidade='$nova_quantidade' WHERE id='$produto_id'";
             mysqli_query($conexao, $sql_update_estoque);
 
             // Inserir registro de saída por venda
-            $sql = "INSERT INTO saida_venda (cliente_id, produto_id, quantidade, valor_unitario) VALUES ('$cliente_id', '$produto_id', '$quantidade', '$valor_unitario')";
-            if (mysqli_query($conexao, $sql)) {
-                $mensagem = "Registro de saída por venda realizado com sucesso!";
+            $sql_saida_venda = "INSERT INTO saida_venda (cliente_id, produto_id, quantidade, valor_unitario) VALUES ('$cliente_id', '$produto_id', '$quantidade', '$valor_unitario')";
+            if (mysqli_query($conexao, $sql_saida_venda)) {
+                // Inserir registro na entrada_financa
+                $sql_entrada_financa = "INSERT INTO entrada_financa (tipo, valor, usuario_id, descricao) VALUES ('Venda de Produto/Item', '$valor_total', '$usuario_id', '$descricao_financa')";
+                mysqli_query($conexao, $sql_entrada_financa);
+
+                // Obter o nome do usuário
+                $query_usuario = "SELECT nome FROM usuario WHERE id = '$usuario_id'";
+                $resultado_usuario = mysqli_query($conexao, $query_usuario);
+                $usuario = mysqli_fetch_assoc($resultado_usuario);
+                $nome_usuario = $usuario ? $usuario['nome'] : 'Nome não encontrado';
+
+                // Montar a mensagem para o Discord - Registro de Venda
+                $mensagem_discord_venda = "```md\n";
+                $mensagem_discord_venda .= "######  Registro de Venda  ######\n\n";
+                $mensagem_discord_venda .= "## Produto:\n";
+                $mensagem_discord_venda .= "- " . $nome_produto . "\n";
+                $mensagem_discord_venda .= "## Nome do Cliente:\n";
+                $mensagem_discord_venda .= "- " . $nome_cliente . "\n";
+                $mensagem_discord_venda .= "## Quantidade Vendida:\n";
+                $mensagem_discord_venda .= "- " . $quantidade . "\n";
+                $mensagem_discord_venda .= "## Quantidade Anterior no Estoque:\n";
+                $mensagem_discord_venda .= "- " . $produto['quantidade'] . "\n";
+                $mensagem_discord_venda .= "## Quantidade Depois da Venda:\n";
+                $mensagem_discord_venda .= "- " . $nova_quantidade . "\n";
+                $mensagem_discord_venda .= "## Valor /Un do Produto:\n";
+                $mensagem_discord_venda .= "- $ " . number_format($valor_unitario, 0, '.', '.') . " USD\n";
+                $mensagem_discord_venda .= "## Valor Total da Venda:\n";
+                $mensagem_discord_venda .= "- R$ " . number_format($valor_total, 0, '.', '.') . " USD\n";
+                $mensagem_discord_venda .= "## Quem vendeu:\n";
+                $mensagem_discord_venda .= "- " . $nome_usuario . "\n";
+                $mensagem_discord_venda .= "## Data e Hora:\n";
+                $mensagem_discord_venda .= "- " . date('d/m/Y H:i:s') . "\n";
+                $mensagem_discord_venda .= "```";
+
+                // Webhook do Discord para registro de venda
+                $webhook_url_venda = 'https://discord.com/api/webhooks/1244999970046607532/33GawFPoeuJ-6yrkQX5_krlgLADMYZXcBwbC-czlYlh6BHfZVGyLJIm7k4p16pid9qGO';
+                $data_discord_venda = json_encode(['content' => $mensagem_discord_venda]);
+
+                $ch_venda = curl_init($webhook_url_venda);
+                curl_setopt($ch_venda, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+                curl_setopt($ch_venda, CURLOPT_POST, 1);
+                curl_setopt($ch_venda, CURLOPT_POSTFIELDS, $data_discord_venda);
+                curl_setopt($ch_venda, CURLOPT_RETURNTRANSFER, true);
+                curl_exec($ch_venda);
+                curl_close($ch_venda);
+
+                // Montar a mensagem para o Discord - Registro de Entrada de Dinheiro
+                $mensagem_discord_entrada = "```md\n";
+                $mensagem_discord_entrada .= "######  Registro de Entrada de Dinheiro  ######\n\n";
+                $mensagem_discord_entrada .= "## Tipo:\n";
+                $mensagem_discord_entrada .= "- Venda de Produto/Item\n";
+                $mensagem_discord_entrada .= "## Descrição:\n";
+                $mensagem_discord_entrada .= "- " . $descricao_financa . "\n";
+                $mensagem_discord_entrada .= "## Valor da Venda:\n";
+                $mensagem_discord_entrada .= "- $ " . number_format($valor_total, 0, '.', '.') . " USD\n";
+                $mensagem_discord_entrada .= "## Quem vendeu:\n";
+                $mensagem_discord_entrada .= "- " . $nome_usuario . "\n";
+                $mensagem_discord_entrada .= "## Data e Hora:\n";
+                $mensagem_discord_entrada .= "- " . date('d/m/Y H:i:s') . "\n";
+                $mensagem_discord_entrada .= "```";
+
+                // Webhook do Discord para registro de entrada de dinheiro
+                $webhook_url_entrada = 'https://discord.com/api/webhooks/1244829573711925301/U4Y-SOK107N8ANfS96VF1qIOkYwWqNCwN_eTZKfcwrBmuHpED4mkOdubfUHWFHxCOHM_';
+                $data_discord_entrada = json_encode(['content' => $mensagem_discord_entrada]);
+
+                $ch_entrada = curl_init($webhook_url_entrada);
+                curl_setopt($ch_entrada, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+                curl_setopt($ch_entrada, CURLOPT_POST, 1);
+                curl_setopt($ch_entrada, CURLOPT_POSTFIELDS, $data_discord_entrada);
+                curl_setopt($ch_entrada, CURLOPT_RETURNTRANSFER, true);
+                curl_exec($ch_entrada);
+                curl_close($ch_entrada);
+
+                $mensagem = "Registro de saída por venda realizado com sucesso! Estoque atual: $nova_quantidade";
             } else {
                 $mensagem = "Erro ao registrar saída por venda: " . mysqli_error($conexao);
             }
         } else {
-            // Exibir a mensagem de erro com a quantidade atual do estoque
             $mensagem = "Quantidade solicitada ($quantidade) indisponível em estoque (Atual: {$produto['quantidade']})!";
         }
     } else {
@@ -69,8 +150,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <!-- redireciona para a tela indicada -->
                 </div>
             </div>
-                        <!-- COLUNA 2 -->
-                        <div class="col-md-7 p-5 ">
+            <!-- COLUNA 2 -->
+            <div class="col-md-7 p-5">
                 <h2 class="text-center" style="color: #EE82EE;"> <i class="far fa-copy"></i> Registrar Venda</h2>
                 <hr class="mb-5">
                 <!-- Ele envia os dados para tela de cadastro-->
@@ -110,7 +191,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             <input type="number" class="form-control" name="quantidade" id="quantidade" aria-describedby="quantidade" placeholder="Quantidade" required>
                             <span id="mensagem_estoque" class="text-danger"><?php echo $mensagem; ?></span>
                             <?php if ($mensagem) { // Exibir a quantidade atual do estoque apenas se houver mensagem de erro ?>
-                                <span class="text-muted">Estoque atual: <?php echo $produto['quantidade']; ?></span>
+                                <span class="text-muted">Estoque anterior: <?php echo $produto['quantidade']; ?></span>
                             <?php } ?>
                         </div>
                     </div>
@@ -145,50 +226,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaI6xoAR1p8FN1RtM0rR8tjt+fJ59b116Y9+N" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+d3L9aijxdh58bDr7amv8AwtIAXUHsE3wYRsM1P6Vx9W8fwtBQ" crossorigin="anonymous"></script>
     <script>
-        function verificarEstoque() {
-            var produto_id = document.getElementById("produto_id").value;
-            var quantidade = document.getElementById("quantidade").value;
+        // Atualizar o valor unitário automaticamente ao selecionar o produto
+        $('#produto_id').change(function() {
+            var valor = $(this).find(':selected').data('valor');
+            $('#valor_unitario').val(valor);
+            atualizarValorTotal();
+        });
 
-            // Requisição AJAX para verificar o estoque em tempo real
-            $.get("verificar_estoque.php", {
-                produto_id: produto_id,
-                quantidade: quantidade
-            }, function(data) {
-                var resposta = JSON.parse(data);
-                if (!resposta.disponivel) {
-                    document.getElementById("mensagem_estoque").innerHTML = "Quantidade indisponível no estoque!";
-                } else {
-                    document.getElementById("mensagem_estoque").innerHTML = "";
-                    document.getElementById("form_registro_venda").submit(); // Enviar o formulário se a quantidade estiver disponível
-                }
-            });
+        // Atualizar o valor total automaticamente ao alterar a quantidade ou o valor unitário
+        $('#quantidade, #valor_unitario').on('input', function() {
+            atualizarValorTotal();
+        });
 
-            return false; // Evitar o envio do formulário antes da resposta da requisição AJAX
+        function atualizarValorTotal() {
+            var quantidade = parseFloat($('#quantidade').val());
+            var valor_unitario = parseFloat($('#valor_unitario').val());
+            var valor_total = quantidade * valor_unitario;
+            if (!isNaN(valor_total)) {
+                $('#valor_total').text(valor_total.toFixed(2));
+            } else {
+                $('#valor_total').text('');
+            }
         }
-
-        function calcularValorTotal() {
-            var quantidade = parseInt(document.getElementById("quantidade").value);
-            var valorUnitario = parseFloat(document.getElementById("valor_unitario").value);
-
-            var valorTotal = quantidade * valorUnitario;
-
-            // Formatando o valor total em dólares
-            var valorTotalFormatado = valorTotal.toLocaleString('en-US', {
-                style: 'currency',
-                currency: 'USD'
-            });
-
-            document.getElementById("valor_total").innerHTML = valorTotalFormatado;
-        }
-
-        // Chamando a função calcularValorTotal() quando houver alterações nos campos de quantidade ou valor unitário
-        document.getElementById("quantidade").addEventListener("input", calcularValorTotal);
-        document.getElementById("valor_unitario").addEventListener("input", calcularValorTotal);
     </script>
 </body>
-
 </html>
